a817e7349e86fabbfaa16f116cc9a6fb7a296be0
lastchance2737 <paveltuska@gmail.com>
2023-02-01 23:40:02 +0300

    Add "run" to build.gradle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting() {
         System.out.print("May I have your name? ");
+
         Scanner scanner = new Scanner(System.in);
         var name = scanner.nextLine();
+        scanner.close();
+
         System.out.println("Hello, " + name + "!");
     }
 }
